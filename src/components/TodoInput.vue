c59aee8918791a602f6667aa7075ccba6134d895
giy <giy.hands@gmail.com>
2023-01-10 14:06:22 +0900

    misc

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -6,7 +6,7 @@
         </span>
 
         <modal v-if="showModal" @close="showModal = false">
-            <h3 slot="header">경고</h3>
+            <h3 slot="header">경고!</h3>
             <span slot="footer" @click="showModal = false">
                 할 일을 입력하세요.
                 <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
